ed004be615698c5f0279c86e50af32ad0828bfe8
Akhil Bhalerao <iamakhil1408@gmail.com>
2022-06-04 16:18:59 +0530

    first commit

---
+++ components/DetailsBid.js
@@ -0,0 +1,45 @@
+import { View, Text, Image } from 'react-native'
+import { EthPrice } from "./SubInfo"
+import { COLORS, SIZES, FONTS } from "../constants"
+
+
+const DetailsBid = ({ bid }) => {
+  return (
+    <View style={{
+        width: "100%",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignContent: "center",
+        marginVertical: SIZES.base,
+        paddingHorizontal: SIZES.base * 2
+    }}>
+      <Image 
+        source={bid.image}
+        resizeMode="contain"
+        style={{ width: 48, height: 48 }}
+      />
+      <View>
+          <Text style={{
+              fontWeight: "600",
+              fontSize: SIZES.font,
+              color: COLORS.primary,
+          }}>
+              Bid placed by {bid.name}
+          </Text>
+          <Text style={{
+              fontSize: SIZES.small - 2,
+              color: COLORS.secondary,
+              marginTop: 3,
+
+          }}>
+              {bid.date}
+          </Text>
+      </View>
+
+      <EthPrice price={bid.price} />
+
+    </View>
+  )
+}
+
+export default DetailsBid
---
+++ components/DetailsDesc.js
@@ -0,0 +1,71 @@
+import { View, Text } from 'react-native'
+import { useState } from 'react'
+
+import { EthPrice, NFTTitle } from "./SubInfo";
+import { COLORS, SIZES, FONTS } from "../constants";
+
+const DetailsDesc = ({ data }) => {
+    const [text, setText] = useState(data.description.slice(0, 100));
+    const [readMore, setReadMore] = useState(false);
+  return (
+    <>
+        <View style={{
+            width: "100%",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+        }}>
+            <NFTTitle 
+                title={data.name} 
+                subTitle={data.creator}
+                titleSize={SIZES.extraLarge}
+                subTitleSize={SIZES.font}
+            />
+
+            <EthPrice price={data.price} />
+        </View>
+
+        <View style={{
+            marginVertical: SIZES.extraLarge*1.5,
+        }}>
+            <Text style={{
+                fontWeight: "700",
+                fontSize: SIZES.medium,
+                color: COLORS.primary,
+
+            }}>Description</Text>
+
+            <View style={{ marginTop: SIZES.base }}>
+                <Text style={{
+                    fontSize: SIZES.font,
+                    color: COLORS.secondary,
+                    lineHeight: SIZES.large,
+                }}>{text}
+                {!readMore && "..."}
+                <Text 
+                style={{
+                    fontWeight: "700",
+                    fontSize: SIZES.small,
+                    color: COLORS.primary,
+                }}
+                onPress={() => {
+                    if (!readMore) {
+                        setText(data.description);
+                        setReadMore(true);
+                    } else {
+                        setText(data.description.slice(0, 100));
+                        setReadMore(false);
+                    }
+                }}
+                >
+                    {readMore ? "  Show less": " Read More"}
+                </Text>
+                </Text>
+            </View>
+
+        </View>
+    </>
+  )
+}
+
+export default DetailsDesc
